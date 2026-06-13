3768bd86c5c4a01debba739befc24491b92ffcb3
Kumar Anand Vardhan <kranandv@gmail.com>
2026-06-13 21:44:21 +0530

    db url added

--- database.py
+++ database.py
@@ -2,12 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# PostgreSQL Connection
+DATABASE_URL = os.getenv("postgresql://myadmin:bVY3Qklau21SLPA8YMHEcCdCsH0Rbp5w@dpg-d8mn3d0js32c73ct19ig-a.oregon-postgres.render.com/spendsmart_7a1l")  # Render gives this directly
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,                    # Set True during development
+    pool_pre_ping=True,            # Good for Render
+    pool_size=5,
+    max_overflow=10
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Pgadmin_123@localhost/TodoApplicationDatabase"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Mysql_123@localhost:3306/SpendSmartDatabase"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Mysql_123@localhost:3306/SpendSmartDatabase"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app= FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"],
+    allow_origins=["https://spendsmart-frontend-5q27.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
